81765c2dbee0d63b679a70f4eff5742db627892d
Paddy <p.hiske@web.de>
2021-10-27 09:47:33 +0200

    bla

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,14 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+const path = require("path");
+const {products} = require("./data");
+
+app.get('/:productId', (req, res)=>{
+c
+    res.json(newProducts);
+})
+
+
+app.listen(5000, () => {
+    console.log("Server läuft");
+})
